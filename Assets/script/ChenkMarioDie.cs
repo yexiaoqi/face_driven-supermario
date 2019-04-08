aa103dfcfc0f6f30b6ab1dc175e9529db15bf6af
yexiaoqi <2417192079@qq.com>
2019-04-08 19:53:59 +0800

    test

---
+++ Assets/script/ChenkMarioDie.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChenkMarioDie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < -1.1f)
+        {
+            setMarioDie();
+        }
+	}
+
+
+    void OnGUI()
+    {
+
+    }
+
+    public void setMarioDie()
+    {
+        World.playAudio(World.dead1AudioIndex);
+        //transform.position = new Vector3(-1, 0, 0);
+        //Camera.main.transform.position = new Vector3(0, 0, -10);
+#pragma warning disable CS0618 // 类型或成员已过时
+        Application.LoadLevel("1");
+#pragma warning restore CS0618 // 类型或成员已过时
+    }
+}
---
+++ Assets/script/Flag.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flag : MonoBehaviour {
+
+    private bool isSuccess = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}  
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "mario")
+        {
+            if (!isSuccess)
+            {
+//<<<<<<< HEAD
+               // GetComponent<Rigidbody2D>().gravityScale = 0.02f;
+//=======
+                GetComponent<Rigidbody2D>().gravityScale = 0.02f;
+//>>>>>>> 8c9a1df50fb98a4abbda29382c341737c5fca2fe
+                isSuccess = true;
+            }
+        }
+    }
+}
